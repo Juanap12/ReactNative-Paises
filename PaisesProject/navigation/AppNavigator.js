43772fb72a84de6e924bb1f43e91f8b24c8c83d5
nicorema <nicolas.restrepo.mantilla@gmail.com>
2018-11-01 21:03:27 -0500

    Logged Screen

--- PaisesProject/navigation/AppNavigator.js
+++ PaisesProject/navigation/AppNavigator.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import { createSwitchNavigator } from 'react-navigation';
+import { createSwitchNavigator,createStackNavigator } from 'react-navigation';
+import LoggedIn from '../screens/LoggedIn';
 
 import MainTabNavigator from './MainTabNavigator';
 
@@ -7,4 +8,6 @@ export default createSwitchNavigator({
   // You could add another route here for authentication.
   // Read more at https://reactnavigation.org/docs/en/auth-flow.html
   Main: MainTabNavigator,
-});
+  Login: LoggedIn ,
+
+}); 
