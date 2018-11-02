4e74462b72fb8d058dc43538be3f25e54fe856bd
nicorema <nicolas.restrepo.mantilla@gmail.com>
2018-11-01 22:41:00 -0500

    Rest call

--- PaisesProject/screens/LoggedIn.js
+++ PaisesProject/screens/LoggedIn.js
@@ -1,27 +1,62 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, TextInput, View } from 'react-native';
+import { AppRegistry, Text, TextInput, View, FlatList,TouchableHighlight } from 'react-native';
 
 export default class LoggedIn extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {text: ''};
-  }
+    constructor(props) {
+        super(props);
+        this.state = {
+            loading: false,
+            paises: [],
+            endpoint: 'https://restcountries.eu/rest/v2/all?fields=name'
 
-  render() {
-    return (
-      <View style={{padding: 10}}>
-        <TextInput
-          style={{height: 40}}
-          placeholder="Type here to translate!"
-          onChangeText={(text) => this.setState({text})}
-        />
-        <Text style={{padding: 10, fontSize: 42}}>
-          {this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-        </Text>
-      </View>
-    );
-  }
+        };
+    }
+    componentDidMount() {
+        this.getPaises();
+    }
+
+    getPaises = () => {
+        this.setState({ loading: true })
+        fetch(this.state.endpoint)
+            .then(res => res.json())
+            .then(res => {
+                this.setState({
+                    paises: res,
+                    loading: false
+                })
+            })
+    };
+
+    vewDetail(item){
+        console.log(item.name);
+    }
+    render() {
+        if (this.state.loading || this.state.paises.length == 0) {
+            return (
+                <View style={{ padding: 60 }}>
+                    <Text> Downloading Countries </Text>
+                </View>
+            )
+        } else if (!this.state.loading && this.state.paises.length != 0) {
+            return (
+                <View style={{marginTop:20}}>
+                    <FlatList
+                        data={this.state.paises}
+                        renderItem={({item, separators}) => (
+                            <TouchableHighlight 
+                              onPress={() => this.vewDetail(item)}
+                              onShowUnderlay={separators.highlight}
+                              onHideUnderlay={separators.unhighlight}>
+                              <View style={{backgroundColor: 'white'}}>
+                                <Text style={{ fontSize: 25,margin: 15,}}>{item.name}</Text>
+                              </View>
+                            </TouchableHighlight>
+                          )}
+                          keyExtractor={(item, index) => index.toString()}
+                        />
+                </View>
+            );
+        }
+    }
 }
 
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => PizzaTranslator);
